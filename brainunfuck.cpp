39a9073804f2e196a779e2db929f3471636cb0f8
Nathan Cregut <mcr6114@live.com>
2016-09-04 23:30:50 -0500

    Fixed special cases for looping, added way to keep track of which byte starts loop

--- brainunfuck.cpp
+++ brainunfuck.cpp
@@ -25,18 +25,24 @@ int main(int argc, char const *argv[])
 	}
 
 	// declare variables
-	int bytes[100] = {0};   // Store values for each byte
-	int bitIndex = 0;       // Keep track of where we are in bytes array
-	int loop[100] = {0};    // Keep track of how many times we are looping through a while
-                            // Used for multiplying addition in a while loop
-	int loopIndex = 0;      // Keep track of where we are in the loop array
-	int addsub = 0;         // Used to keep track of how many numbers we are adding/subtracting
+	int bytes[100] = {0};	// Store values for each byte
+	int bitIndex = 0;		// Keep track of where we are in bytes array
+	int loop[100] = {0};	// Keep track of how many times we are looping through a while
+							// Used for multiplying addition in a while loop
+	int loopIndex = 0;		// Keep track of where we are in the loop array
+	int loopstarter[100] = {-1}; // Used to keep track which byte starts a loop
+	int addsub = 0;			// Used to keep track of how many numbers we are adding/subtracting
+	int loops = 0;          // Used to keep track of how many loops are happening in outer loop
+	bool flag = true;		// Used to keep track of whether or not we get another character
+							// At the end of the while loop
 
 	// Grab first character from file
 	int c = ifs.get();
 
 	// continue to run until reached end of file
-	while (c != EOF) {		
+	while (c != EOF) {
+		flag = true;
+		
 		// Check if we are adding
 		if (c == '+') {
 			// add to addsub
@@ -52,11 +58,14 @@ int main(int argc, char const *argv[])
 			if (loop[loopIndex] > 0) {
 				// If so multiply addsub by the times we would loop
 				addsub *= loop[loopIndex];
-				for (int i = loopIndex-1; i >= 0; i--) {
-					// Multiply for every loop we are currently
-					// Inside of
-					addsub *= loop[i];
+				if (loopstarter[loopIndex] != bitIndex && loops < 3) {
+					for (int i = loopIndex-1; i >= 0; i--) {
+						// Multiply for every loop we are currently
+						// Inside of
+						addsub *= loop[i];
+					}
 				}
+				
 			}
 
 			bytes[bitIndex] += addsub;
@@ -80,11 +89,15 @@ int main(int argc, char const *argv[])
 			if (loop[loopIndex] > 0) {
 				// If so multiply addsub by the times we would loop
 				addsub *= loop[loopIndex];
-				for (int i = loopIndex-1; i >= 0; i--) {
-					// Multiply for every loop we are currently
-					// Inside of
-					addsub *= loop[i];
+				if (loopstarter[loopIndex] != bitIndex && loops < 3) {
+					for (int i = loopIndex-1; i >= 0; i--) {
+						// Multiply for every loop we are currently
+						// Inside of
+						addsub *= loop[i];
+					}
 				}
+					
+
 			}
 
 			bytes[bitIndex] -= addsub;
@@ -105,6 +118,7 @@ int main(int argc, char const *argv[])
 			cout << "Moved to index " << bitIndex << endl;
 
 		} else if (c == '<') {
+			flag = false;
 			bitIndex--;
 			while (((c = ifs.get()) != EOF) && c == '<') {
 				// add to bitIndex
@@ -116,12 +130,19 @@ int main(int argc, char const *argv[])
 		} else {
 			// Check to see if starting loop
 			if (c == '[') {
+				loops++;
 				// Check to see if we are already in a loop
-				if (loop[loopIndex] >0) {
+				if (loop[loopIndex] > 0) {
+					if (loops > 2) {
+						bytes[bitIndex] /= loop[loopIndex];
+						cout << "Fixed loop bit number to correct loop count" << endl
+							<< "Index " << bitIndex << " is now " << bytes[bitIndex] << endl;
+					}
 					//Add to loopIndex
 					loopIndex++;
 					//Set loop[loopIndex] to current index
 					loop[loopIndex] = bytes[bitIndex];
+					loopstarter[loopIndex]= bitIndex;
 				} else {
 					//Set loop[loopIndex] to current index
 					loop[loopIndex] = bytes[bitIndex];
@@ -132,13 +153,20 @@ int main(int argc, char const *argv[])
 
 			// Check to see if ending while loop
 			} else if (c == ']') {
-				loop[loopIndex] = 0;
-				
 				// check to see if we were in any other loops
 				if (loopIndex > 0) {
+					loop[loopIndex] = 0;
+					// Reset loopstarter at current index
+					loopstarter[loopIndex] = -1;
 					//Subtract loopIndex
 					loopIndex--;
+
+				} else {
+					loop[loopIndex] = 0;
+					loops = 0;
+					loopstarter[loopIndex] = -1;
 				}
+				
 				//Print out that we ended a loop
 				cout << "Exited while loop." << endl;
 
@@ -151,8 +179,8 @@ int main(int argc, char const *argv[])
 			} else if (c == ',') {
 				// State that inputs are represented as 100
 				cout << "Asking for input from user" << endl
-					<< "Input is handled as '100'" << endl;
-					bytes[bitIndex] = 100;
+					<< "Input is handled as '10'" << endl;
+					bytes[bitIndex] = 10;
 			}
 
 			// Retrieve another character from file
